f641fe1f0349b36a445d94b82e4319ffd83c3585
tanishq <tanishqsinghai11@gmail.com>
2020-10-14 21:51:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'passwordGenerator';
+  includeLetters=false;
+  includeSymbols=false;
+  includeNumbers=false;
+  password='';
+  length=0;
+  onButtonClick(){
+    const numbers='1234567890';
+    const letters='abcdefghijklmnopqrstuvwxyz';
+    const symbols='!@#$%^&*()_+=-<>?';
+    let validChars='';
+    if(this.includeLetters){
+      validChars+=letters;
+    }
+    if(this.includeNumbers){
+      validChars+=numbers;
+    }
+    if(this.includeSymbols){
+      validChars+=symbols;
+    }
+    let finalPassword='';
+    for(let i=0;i<this.length;i++)
+    {
+      const idx = Math.floor(Math.random() * validChars.length);
+      finalPassword += validChars[idx];
+    }
+    this.password = finalPassword;
+  }
+  onCheckUseSymbols(){
+     this.includeSymbols=!this.includeSymbols;
+  }
+  onCheckUseLetters(){
+    this.includeLetters=!this.includeLetters;
+  }
+  onCheckUseNumbers(){
+    this.includeNumbers=! this.includeNumbers;
+  }
+  onLength(value : string){
+    const parsedValue = parseInt(value);
+    if(!isNaN(parsedValue))
+    {
+      this.length = parsedValue;
+    }
+  }
 }
